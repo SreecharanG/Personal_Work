a5cc1973399ad5e02d97ea8bddce50926689daee
SreecharanG <charan.24119@gmail.com>
2018-02-10 20:51:18 -0800

    22 23 24 25 26 27

---
+++ Convert_Sorted_List_To_Binary_Search_Tree.py
@@ -0,0 +1,34 @@
+class ListNode(object):
+	def __init__(self, x):
+		self.val = x
+		self.next = None
+
+class TreeNode(object):
+	def __init_-(self, x):
+		self.val = x
+		self.left = None
+		self.right = None
+
+class Solution(object):
+	
+	def sortedListToBST(self, head):
+		node, length = head, 0
+		while node:
+			node = node.next
+			length += 1
+		self.curr = head
+		return self._sortedListToBST(0, length - 1)
+
+	def _sortedListToBST(self, left, right):
+		if left > right:
+			return None
+
+		mid = (left + right) // 2
+		left = self._sortedListToBST(left, mid - 1)
+		root = TreeNode(self.curr.val)
+		root.left = left
+		self.curr = self.curr.next
+		root.right = self._sortedListToBST(mid + 1, right)
+		return root
+		
+
