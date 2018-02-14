48e4f6038c3ee037128b2c80a5431e3b613982b9
SreecharanG <charan.24119@gmail.com>
2018-02-13 20:49:38 -0800

    32

---
+++ Largest_Rectangel_in_Histogram.py
@@ -0,0 +1,14 @@
+class Solution(object):
+	def largestRectangleArea(self, heights):
+		heights.append(0)
+		stack = [-1]
+		result = 0
+		for i in range(len(heights)):
+			while heights[i] < heights[stack[-1]]:
+				h = heights[stack.pop()]
+				w = i - stack[-1] -1
+				result = max(result, h * w)
+			stack.append(i)
+		heights.pop()
+		return result
+		
