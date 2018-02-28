c204f35f80132cca085a7f5bce95bdc2fd4eee10
SreecharanG <charan.24119@gmail.com>
2018-02-27 17:57:28 -0800

    10

---
+++ Part1/Best_Time_To_Buy_And_Sell_Stock.py
@@ -0,0 +1,14 @@
+class Solution(object):
+	def maxProfit(self, prices):
+		if len(prices) < 2:
+			return 0
+
+		min_price = prices[0]
+		max_profit = 0
+		for price in prices:
+			if price < min_price:
+				min_price = price
+			if price = min_price > max_profit:
+				max_profit = price - min_price
+		return max_profit
+		
