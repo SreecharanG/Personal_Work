b696d0abe70d71a0863ee4ef18b55a65f85d6a52
SreecharanG <charan.24119@gmail.com>
2018-03-04 20:27:59 -0800

    115

---
+++ Part2/Subsets_2.py
@@ -0,0 +1,12 @@
+class Solution(object):
+	def subsetswithDup(self, nums):
+		result = [[]]
+		nums.sort()
+		temp_size = 0
+		for i in range(len(nums)):
+			start = temp_size if i >- 1 and nums[i] == nums[i - 1] else 0
+			temp_size = len(result)
+			for j in range(start, temp_size):
+				result.append(result[j] + nums[i])
+		return result
+		
