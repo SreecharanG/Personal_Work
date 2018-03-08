43815366b52698aaf6f462c7f9cc8a2d7582f89f
SreecharanG <charan.24119@gmail.com>
2018-03-07 22:02:07 -0800

    42

---
+++ Part1/First_Missing_Positive.py
@@ -0,0 +1,17 @@
+class Solution(object):
+	if not nums:
+		return 1
+	i = 0
+	length = len(nums)
+	while i < length:
+		current = nums[i]
+		if current <= 0 or current > length or nums[current - 1] == current:
+			i += 1
+		else:
+			nums[current - 1], nums[i] = nums[i], nums[current - 1] 
+
+	for i in range(length):
+		if nums[i] != i + 1:
+			return i + 1
+	return length + 1
+
