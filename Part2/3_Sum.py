418a80884249f68ed2606648e8aa4c70eb7669b6
SreecharanG <charan.24119@gmail.com>
2018-03-24 18:11:13 -0700

    1

---
+++ Part2/3_Sum.py
@@ -0,0 +1,28 @@
+class Solution(object):
+	def threeSum(self, nums):
+		nums.sort()
+		result =[]
+		i = 0
+		while i < len(nums) - 2:
+			j = i + 1
+			k = len(nums) - 1
+			while j < k:
+				l = [nums[i], nums[j], nums[k]]
+				if sum(l) == 0:
+					result.append(l)
+					j += 1
+					k -= 1
+					while j < k and nums[j] == nums[j - 1]:
+						j += 1
+					while j < k and nums[k] == nums[k + 1]:
+						k -= 1
+				elif sum(l) > 0:
+					k -= 1
+				else:
+					j += 1
+			i += 1
+
+			while i < len(nums) - 2 and nums[i] == nums[i - 1]:
+				i += 1
+		return result
+		
