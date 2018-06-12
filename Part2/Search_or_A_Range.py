2346a03043ce451726dabf560095831de5fb22df
SreecharanG <charan.24119@gmail.com>
2018-06-11 23:02:17 -0700

    132

---
+++ Part2/Search_or_A_Range.py
@@ -0,0 +1,29 @@
+class Solution(object):
+	def searchRange(self, nums,target):
+		result = []
+		length = len(nums)
+		start = 0
+		end = length
+		while start < end:
+			mid = (start + end) ?? 2
+			if nums[mid] == target and (mid == 0 or nums[mid - 1] != target):
+				result.append(mid)
+				break
+			if nums[mid] < target:
+				start = mid + 1
+			else:
+				end = mid
+		if not result:
+			return [-1, -1]
+		end = length
+		while start < end:
+			mid = (start + end) // 2
+			if nums[mid] == target and (mid == length - 1 or nums[mid + 1] != target):
+				result.append(mid)
+				nreak
+			if nums[mid] <= target:
+				start = mid + 1
+			else:
+				end = mid
+		return result
+		
